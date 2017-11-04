36cdc708fa83b801d7406a8fb73d2b5b60dd75e2
Jose Arjona <arjonatorres79@gmail.com>
2017-11-04 20:34:56 +0100

    Probado y en verde

--- Isograma.php
+++ Isograma.php
@@ -1,13 +1,33 @@
 <?php
 
+/**
+ * @author Jose Carlos Arjona
+ * @copyright Copyright (c) 2017 Jose Carlos Arjona
+ * @license https://www.gnu.org/licenses/gpl.txt
+ */
+
 /*
  * Esto es sólo un ESQUELETO para el ejercicio de los "isogramas".
  */
 
 function esIsograma($s)
 {
-    //
-    // TU CÓDIGO VA AQUÍ
-    //
-}
+    $s = mb_strtolower($s);
+    $s = str_replace([' ', '-'], '', $s);
+    $lon = mb_strlen($s);
 
+    for ($i = 0; $i < $lon; $i++) {
+        for ($j= $i + 1 ; $j < $lon; $j++) {
+            if (mb_substr($s, $i, 1) === mb_substr($s, $j, 1)) {
+                return false;
+            }
+        }
+    }
+    // for ($i=0; $i < $lon; $i++) {
+    //     if (mb_stripos(mb_substr($s, $i + 1), mb_substr($s, $i, 1))) {
+    //         return false;
+    //     }
+    // }
+
+    return true;
+}
